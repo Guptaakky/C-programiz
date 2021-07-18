19b9676c86ad623e2430e64ac5a2e612229f29d6
Akshat gupta <83388555+Guptaakky@users.noreply.github.com>
2021-07-18 22:54:31 +0530

    Create Create bitwise right shift using while loop.c

---
+++ Create bitwise right shift using while loop.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+ int i,k;
+ int j=1;
+ printf("This is designed by  Akshat gupta \n");
+ printf("Enter any integer:");    
+ scanf("%d",&i);
+
+
+ while(j<3)
+ {
+  k=i>>j;
+  printf("\n The value after right shifting is %d",k);
+  j++;
+ }
+ return 0;

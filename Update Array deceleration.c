6af1f11c2303283ab7c649ae146c3f821e66c9a3
Akshat gupta <83388555+Guptaakky@users.noreply.github.com>
2021-07-18 23:00:14 +0530

    Create Update Array deceleration.c

---
+++ Update Array deceleration.c
@@ -0,0 +1,7 @@
+{
+
+ int m[5]={5,8,2,0,1};
+ printf("This is designed by Akshat gupta  ");
+ printf("The numbers are %d \n",m[0]);
+ printf("The numbers are %d \n",m[1]);
+ printf("The numbers are %d \n",m[2]);
